a3cc377d85cf17983285c55d94604f0e15c3bfcb
Abhayraj-Jha <89918644+Abhayraj-Jha@users.noreply.github.com>
2024-02-15 11:35:34 +0530

    feat: added country field

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
 import streamlit as st
 import requests
+import json
+
+with open("countryCode.json") as f:
+    data = json.load(f)
+
+country_code_list = []
+
+for obj in data:
+    country_code_str = f"""{obj["dial_code"]} - {obj["name"]}"""
+    country_code_list.append(country_code_str)
 
 st.header(body="User details", divider="rainbow")
 st.markdown(
@@ -13,6 +23,9 @@ first_name = st.text_input(label="First name", placeholder="Ajay, Shubham")
 
 last_name = st.text_input(label="Last name", placeholder="kumar, Singh")
 
+
+country_code_value = st.selectbox("Select country code", country_code_list)
+
 phone_no = st.text_input(label="Phone no", placeholder="9999999999, 7896999999")
 st.write("##")
 
@@ -20,10 +33,15 @@ send_button = st.button(label="SEND", type="primary")
 
 
 if send_button:
+
+    parts = country_code_value.split(" - ")
+    country_code = parts[0]
+
     user_infro = {
         "first_name": first_name,
         "last_name_part1": last_name,
         "phone_no": phone_no,
+        "country_code": country_code,
     }
     st.write(user_infro)
     server_url = "http://localhost:3001"
